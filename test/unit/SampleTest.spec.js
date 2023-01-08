4ba282ce0f97cc804a3319e197ff3a16d7c9518b
Ali Khattab <ali.khattab@digital.homeoffice.gov.uk>
2023-01-08 13:05:21 +0000

    adding sample test

---
+++ test/unit/SampleTest.spec.js
@@ -0,0 +1,15 @@
+import {expect} from "chai";
+
+
+
+describe('Sample test suite', () => { 
+
+    it('can run a sample test', async () => {
+
+        const sampleVariable = 2;
+    
+        expect(sampleVariable).to.be.eq(2);
+    });
+    
+})
+
